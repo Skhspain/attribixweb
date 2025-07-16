e76ec24e47daa0ba974e219210c75db5bdd19ba1
Skhspain <stiankhenriksen@gmail.com>
2025-07-16 16:55:47 +0200

    Your message for today's work

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,15 @@
 // src/app/layout.tsx
-import React from 'react'
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'AttribixWeb',
-  description: 'Your dashboard and analytics app',
-}
+  title: "Attribix",
+  description: "Smarter Attribution. Bigger Impact.",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head />
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
